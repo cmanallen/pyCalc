ace231156c42ee1c2e00d88289b34e7bfa6525c6
Colton Allen <cmanallen90@gmail.com>
2014-09-25 20:10:57 -0400

    Updated spacing in the file

--- pyCalc.py
+++ pyCalc.py
@@ -1,5 +1,5 @@
 def pyCalc():
-	if parCheck(equation) == True:
+	if parCheck(equation):
 		solution = postfixSolver(infixToPostfix(equation))
 		print(solution)
 
@@ -13,21 +13,14 @@ def parCheck(string):
 	index = 0
 
 	while index < len(string) and balanced:
-
 		symbol = string[index]
-		
 		if symbol == "(":
 			stack.append(symbol)
-		
 		elif symbol == ")":
 			if not stack:
 				balanced = False
 			else:
 				stack.pop()
-
-		else:
-			pass
-
 		index += 1
 
 	if balanced and not stack:
@@ -37,7 +30,6 @@ def parCheck(string):
 
 
 def infixToPostfix(string):
-
 	prec = {}
 	prec["^"] = 4
 	prec["*"] = 3
@@ -48,36 +40,23 @@ def infixToPostfix(string):
 
 	operator_stack = []
 	output_queue = []
-	
 	string = string.split()
 
 	for element in string:
-		
 		if element not in "()+-*/^":
 			output_queue.append(element)
-
 		elif element == "(":
 			operator_stack.append(element)
-
 		elif element in "+-*/^" and not operator_stack:
 			operator_stack.append(element)
-
 		elif element in "+-*/^" and operator_stack:
-			
 			while operator_stack and prec[operator_stack[-1]] >= prec[element]:
 				output_queue.append(operator_stack.pop())
-			
 			operator_stack.append(element)
-
-		elif element == ")":			
-			
+		elif element == ")":
 			while prec[operator_stack[-1]] > 1:
 				output_queue.append(operator_stack.pop())
-			
 			operator_stack.pop()
-
-		else:
-			pass
 
 	while operator_stack:
 		output_queue.append(operator_stack[-1])
@@ -87,46 +66,38 @@ def infixToPostfix(string):
 
 
 def postfixSolver(string):
-
 	string = string.split()
 	operand_stack = []
 
 	for element in string:
 		if element not in "+-*/^":
 			operand_stack.append(element)
-
 		elif element == "+":
 			new_element = float(operand_stack[-2]) + float(operand_stack[-1])
 			operand_stack.pop()
 			operand_stack.pop()
 			operand_stack.append(new_element)
-
 		elif element == "-":
 			new_element = float(operand_stack[-2]) - float(operand_stack[-1])
 			operand_stack.pop()
 			operand_stack.pop()
 			operand_stack.append(new_element)
-
 		elif element == "*":
 			new_element = float(operand_stack[-2]) * float(operand_stack[-1])
 			operand_stack.pop()
 			operand_stack.pop()
 			operand_stack.append(new_element)
-
 		elif element == "/":
 			new_element = float(operand_stack[-2]) / float(operand_stack[-1])
 			operand_stack.pop()
 			operand_stack.pop()
 			operand_stack.append(new_element)
-
 		elif element == "^":
 			new_element = float(operand_stack[-2]) ** float(operand_stack[-1])
 			operand_stack.pop()
 			operand_stack.pop()
 			operand_stack.append(new_element)
-
 	return operand_stack
-
 
 
 print("Welcome to PyCalc 0.0.0.0.0.2")
